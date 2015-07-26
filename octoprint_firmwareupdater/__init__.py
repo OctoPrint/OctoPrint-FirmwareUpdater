c80b7947ae841f5defc2b19accc06ab65688f265
Nicanor Romero Venier <nik_romero@hotmail.com>
2015-07-26 21:51:45 +0200

    Added configuration menu.

--- octoprint_firmwareupdater/__init__.py
+++ octoprint_firmwareupdater/__init__.py
@@ -9,22 +9,27 @@ from __future__ import absolute_import
 # Take a look at the documentation on what other plugin mixins are available.
 
 import flask
+import os
 import shutil
 
 import octoprint.plugin
-
-from octoprint.settings import settings
+import octoprint.settings
 
 class FirmwareupdaterPlugin(octoprint.plugin.BlueprintPlugin,
 							octoprint.plugin.TemplatePlugin,
 							octoprint.plugin.AssetPlugin,
 							octoprint.plugin.SettingsPlugin):
 
+	#~~ Asset API
+
 	def get_assets(self):
 		return dict(js=["js/firmwareupdater.js"])
 
+	#~~ BluePrint API
+
 	@octoprint.plugin.BlueprintPlugin.route("/uploadHexFile", methods=["POST"])
 	def flash_firmware(self):
+
 		print "Values = ", flask.request.values
 
 		input_name = "file"
@@ -32,10 +37,35 @@ class FirmwareupdaterPlugin(octoprint.plugin.BlueprintPlugin,
 		input_upload_path = input_name + "." + self._settings.global_get(["server", "uploads", "pathSuffix"])
 
 		temp_path = flask.request.values[input_upload_path]
-		dest_path = os.path.join(self._basefolder, "uploads", "hexFile.hex")
-		shutil.copyfile(os.path.abspath(temp_path), dest_path)
+		dest_path = os.path.join(self._settings._basedir, "plugins", "FirmwareUpdater")
 
-		return flask.make_response("Ok.", 200)
+		if not os.path.exists(dest_path):
+			try:
+				os.mkdir(dest_path, 0775)
+				self._logger.info("Creating directory for FirmwareUpdater plugin at {path}".format(path=dest_path))
+			except Exception as e:
+				self._logger.exception("Error when creating directory for FirmwareUpdater plugin: {error}".format(error=e))
+				return flask.make_response("Error.", 500)  # TODO: Change this
+
+		try:
+			shutil.copyfile(os.path.abspath(temp_path), os.path.join(dest_path, "tempHexFile.hex"))
+		except Exception as e:
+			self._logger.exception("Error when copying uploaded temp hex file: {error}".format(error=e))
+
+		return flask.make_response("Ok.", 200)  # TODO: Change this
+
+	#~~ SettingsPlugin API
+
+	def get_settings_defaults(self):
+		return {
+			"path_avrdude": None,
+			"path_avrdudeconfig": None
+		}
+
+	def on_settings_save(self, data):
+		for key in self.get_settings_defaults():
+			if key in data:
+				self._settings.set([key], data[key])
 
 __plugin_name__ = "Firmware Updater"
 
--- octoprint_firmwareupdater/static/js/firmwareupdater.js
+++ octoprint_firmwareupdater/static/js/firmwareupdater.js
@@ -2,11 +2,17 @@ $(function() {
     function FirmwareUpdaterViewModel(parameters) {
         var self = this;
 
+        self.settings = parameters[0];
+
         self.hexFileName = ko.observable();
         self.hexFileURL = ko.observable();
+        self.config_path_avrdude = ko.observable();
+        self.config_path_avrdudeconfig = ko.observable();
 
         self.uploadFirmware = $("#settings-firmwareupdater-upload");
         self.flashFirmware = $("#settings-firmwareupdater-start");
+
+        self.configurationDialog = $("#settings_plugin_firmwareupdater_configurationdialog");
 
         self.uploadFirmware.fileupload({
             dataType: "hex",
@@ -17,17 +23,42 @@ $(function() {
                     return false;
                 }
 
+                self.hexFileName(data.files[0].name);
+
                 self.flashFirmware.unbind("click");
                 self.flashFirmware.on("click", function() {
                     data.submit();
                 });
             }
-        })
+        });
+
+        self.showPluginSettings = function() {
+            self._copyConfig();
+            self.configurationDialog.modal();
+        };
+
+        self.savePluginSettings = function() {
+            var data = {
+                plugins: {
+                    firmwareupdater: {
+                        path_avrdude: self.config_path_avrdude(),
+                        path_avrdudeconfig: self.config_path_avrdudeconfig()
+                    }
+                }
+            };
+            self.settings.saveData(data, function() { self.configurationDialog.modal("hide"); self._copyConfig(); });
+        };
+
+        self._copyConfig = function() {
+            self.config_path_avrdude(self.settings.settings.plugins.firmwareupdater.path_avrdude());
+            self.config_path_avrdudeconfig(self.settings.settings.plugins.firmwareupdater.path_avrdudeconfig());
+        };
+
     };
 
     OCTOPRINT_VIEWMODELS.push([
         FirmwareUpdaterViewModel,
-        [],
-        ["#settings_plugin_firmwareupdater"]
+        ["settingsViewModel"],
+        document.getElementById("settings_plugin_firmwareupdater")
     ]);
 });
