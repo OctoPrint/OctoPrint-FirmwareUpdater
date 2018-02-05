3ed6662ea7221f0e99864c918f663a02c5c2c5c7
Ben Lye <ben@lye.co.nz>
2018-02-05 16:24:49 +0000

    Add capability to reset the board using by opening the serial port at 1200bps.

--- octoprint_firmwareupdater/__init__.py
+++ octoprint_firmwareupdater/__init__.py
@@ -8,6 +8,8 @@ import os
 import requests
 import tempfile
 import threading
+import serial
+from serial import SerialException
 
 import octoprint.plugin
 
@@ -366,6 +368,11 @@ class FirmwareupdaterPlugin(octoprint.plugin.BlueprintPlugin,
 			bossac_command += ["-v"]
 		bossac_command += ["-b", firmware, "-R"]
 
+		self._logger.info(u"Attempting to reset the board")
+		if not self._reset_1200(printer_port):
+			self._logger.error(u"Reset failed")
+			return False
+
 		import sarge
 		self._logger.info(u"Running %r in %s" % (' '.join(bossac_command), working_dir))
 		self._console_logger.info(" ".join(bossac_command))
@@ -434,6 +441,21 @@ class FirmwareupdaterPlugin(octoprint.plugin.BlueprintPlugin,
 			return False
 		else:
 			return True
+
+	def _reset_1200(self, printer_port=None):
+		assert(printer_port is not None)
+		self._logger.info(u"Toggling '{port}' at 1200bps".format(port=printer_port))
+		try:
+			ser = serial.Serial(printer_port, 1200)
+			ser.close()
+			ser.open()
+			ser.close()
+		except SerialException as ex:
+			self._logger.exception(u"Board reset failed: {error}".format(error=str(ex)))
+			self._send_status("flasherror", message="Board reset failed")
+			return False
+		
+		return True
 
 	#~~ SettingsPlugin API
 
