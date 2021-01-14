f9abc7ceea05554782ff80365d09fd4134c520d3
Ben Lye <blye@mathworks.com>
2021-01-14 15:17:53 +0000

    Add pyserial dependency

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/OctoPrint/OctoPrint-FirmwareUpdater"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["pyserial>=3.5"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
